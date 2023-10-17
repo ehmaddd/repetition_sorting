22a59b20ab2707103654b57dd7ab9f44b3f0cd46
ehmaddd <ehmaddd@gmail.com>
2023-10-17 22:33:24 +0500

    Add change

--- Solution.js
+++ Solution.js
@@ -1,22 +1,23 @@
 function sortByFrequency(arr) {
     const frequencyMap = {};
-    arr.forEach((num) => {
-      frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+    arr.map((element)=> {
+      frequencyMap[element] = (frequencyMap[element] || 0) + 1;
     });
 
-    arr.sort((a, b) => {
-        const freqA = frequencyMap[a];
-        const freqB = frequencyMap[b];
+    console.log(frequencyMap);
+    // arr.sort((a, b) => {
+    //     const freqA = frequencyMap[a];
+    //     const freqB = frequencyMap[b];
 
-        // Sort by frequency in ascending order
-        if (freqA !== freqB) {
-          return freqA - freqB;
-        } else {
-          // If frequencies are the same, sort in descending order
-          return b - a;
-        }
-    });
-    return arr;
+    //     // Sort by frequency in ascending order
+    //     if (freqA !== freqB) {
+    //       return freqA - freqB;
+    //     } else {
+    //       // If frequencies are the same, sort in descending order
+    //       return b - a;
+    //     }
+    // });
+    // return arr;
   }
 
   const numbers = [4, 2, 2, 8, 4, 6, 4, 8, 2, 4, 4, 6, 10];
